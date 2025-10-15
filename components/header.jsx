e7f0db7d8fc5e9473af814caf6f28bac858acffe
Md Miskatul Masabi <masabimiskat@gmail.com>
2025-10-15 16:45:05 +0600

    add header sing-up page

---
+++ components/header.jsx
@@ -0,0 +1,22 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 w-full max-w-3xl px-4">
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Header;
